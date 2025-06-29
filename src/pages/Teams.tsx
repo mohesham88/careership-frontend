a5f0a5bb956bd1364e20cf161e5956321dba0d8a
Omar <omar1752003@gmail.com>
2025-06-30 00:21:25 +0300

    Remove console log from Teams component to clean up code and improve performance.

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -17,7 +17,6 @@ export default function Teams() {
       .then(res => {
         setTeams(res.data.results || res.data);
         setLoading(false);
-        console.log(res.data);
       })
       .catch(() => {
         setError('Failed to load teams');
